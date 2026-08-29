b8c504aa2d4c72058a4f9cc4dca032b5c41fd0d4
evamariyaa266-png <evamariyaa266@gmail.com>
2026-08-29 16:28:39 +0530

    Create pluginsCore.js

---
+++ plugins/pluginsCore.js
@@ -0,0 +1,12 @@
+const fs = require('fs');
+const path = require('path');
+
+// Core functions for plugins
+const loadPlugins = () => {
+    // Basic plugin loader handler
+    return true;
+};
+
+module.exports = {
+    loadPlugins
+};
