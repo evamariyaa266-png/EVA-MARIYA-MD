d4cdcfa872f62750ce68d0aed9a1d11f5d1d4d7f
evamariyaa266-png <evamariyaa266@gmail.com>
2026-08-29 12:22:58 +0530

    Create config.js

---
+++ config.js
@@ -0,0 +1,9 @@
+module.exports = {
+    PREFIX: '.',
+    BOT_NAME: 'ᴇᴠᴀ-ᴍᴀʀɪʏᴀ🕊️',
+    OWNER_NAME: '𝑨𝒓𝒋𝒖𝒖🧃',
+    OWNER_NUMBER: '918086460391',
+    MODE: 'public',
+    MENU_IMAGE: 'https://files.catbox.moe/svk9e1.jpg'
+};
+
